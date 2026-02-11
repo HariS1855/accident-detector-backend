776ea7ed1178ec392651488b515e58da71eba0ef
Rakesh-Kumar-M-M <rakesh883872@gmail.com>
2026-02-11 23:28:56 +0530

    Update VoiceController.java

--- src/main/java/com/backend/accidentdetector/controller/VoiceController.java
+++ src/main/java/com/backend/accidentdetector/controller/VoiceController.java
@@ -1,3 +1,8 @@
+package com.backend.accidentdetector.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class VoiceController {
 
